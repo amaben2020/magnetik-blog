90196ad03a2ee46502371e62dd79336041ced303
algomachine007 <amaben202phantom@gmail.com>
2023-09-08 15:38:09 +0100

    build fix

--- src/app/utils/api/user/follow-user.ts
+++ src/app/utils/api/user/follow-user.ts
@@ -15,6 +15,9 @@ export const followUser = async (userId: any, followUserId: any) => {
         throw new Error("User not found.");
       }
 
+      // follower: the user that initiated the follow (person that clicked the follow button) if a follower
+      // following: the user followed by someone i.e i'm following someone
+
       const follow = await prisma.follows.create({
         data: {
           follower: { connect: { id: user.id } },
