a233ecd1eac38f8a13e1410e58232537bacddb40
algomachine007 <amaben202phantom@gmail.com>
2023-09-17 07:52:51 +0100

    refactoring arrraysAreEqual functionality

--- src/app/utils/arrays-are-equals.ts
+++ src/app/utils/arrays-are-equals.ts
@@ -1,16 +1,21 @@
-export function arraysAreEqual(arr1: string[], arr2: string[]) {
-  // early return
+export const arraysAreEqual = (
+  arr1 = [] as string[],
+  arr2 = [] as string[],
+) => {
+  // we need to sort so that no matter the order, it works fine
+  arr1.sort();
+  arr2.sort();
   if (arr1.length !== arr2.length) {
     return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    console.log(arr1[i]);
-    console.log("arr2", arr2[i]);
-    if (arr1[i] === arr2[i]) {
+    const firstArray = arr1[i];
+    const secondArray = arr2[i];
+    if (firstArray === secondArray) {
       return true;
     }
-  }
 
-  return false;
-}
+    return false;
+  }
+};
