36384376f569d22c87d63ff4cc797c35febd9320
algomachine007 <amaben202phantom@gmail.com>
2023-09-18 21:28:20 +0100

    cf setup

---
+++ src/app/lib/contentful/index.ts
@@ -0,0 +1,34 @@
+import { createClient } from "contentful";
+
+// initialize the client
+const {
+  CONTENTFUL_SPACE_ID,
+  CONTENTFUL_ACCESS_TOKEN,
+  CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+} = process.env;
+if (
+  !CONTENTFUL_ACCESS_TOKEN ||
+  !CONTENTFUL_SPACE_ID ||
+  CONTENTFUL_PREVIEW_ACCESS_TOKEN
+) {
+  throw new Error("Please insert env.local Contentful API keys");
+}
+
+export const client = createClient({
+  space: CONTENTFUL_SPACE_ID!,
+  accessToken: CONTENTFUL_ACCESS_TOKEN!,
+});
+
+const previewClient = createClient({
+  space: CONTENTFUL_SPACE_ID!,
+  accessToken: CONTENTFUL_PREVIEW_ACCESS_TOKEN!,
+  host: "preview.contentful.com",
+});
+
+export default function contentfulClient({ preview = false }) {
+  if (preview) {
+    return previewClient;
+  }
+
+  return client;
+}
