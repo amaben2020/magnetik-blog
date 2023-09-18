b1b0dc9c9d01a89583cb0ad2da8c0862c4f93935
algomachine007 <amaben202phantom@gmail.com>
2023-09-18 18:04:13 +0100

    build fix

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,7 +2,7 @@ import { asyncWrapper } from "@/app/utils/api/async-wrapper";
 import { getUser } from "@/app/utils/api/user/get-user";
 import { NextResponse } from "next/server";
 
-export const GET = async (req: NextResponse) => {
+export const GET = async (req: Request) => {
   const reqUrl = new URL(req.url);
   const clerkID = reqUrl.searchParams.get("clerkID");
   const [user, errorUsers] = await asyncWrapper(getUser, clerkID);
